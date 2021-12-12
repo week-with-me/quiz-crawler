9dbaa80e03018fb6908c48b35cd31930f8a04b9b
2_taehyun <63915557+0417taehyun@users.noreply.github.com>
2021-12-12 18:18:09 +0900

    :sparkles: Add Kobis OpenAPI

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from src.util import get_movie_details, get_movie_information
 def lambda_handler(event, context):
     movies = get_movie_information(OPTION='PRODUCTION')
     
+    for movie in movies:
+        movie_info = get_movie_details(movie['movie_cd'])
+        movie.update(movie_info)
+    
     return
 
 
@@ -12,8 +16,4 @@ if __name__ == '__main__':
     for movie in movies:
         movie_info = get_movie_details(movie['movie_cd'])
         movie.update(movie_info)
-        
-    for movie in movies:
-        print(movies)
-    
     
--- src/util/openAPI/kobis.py
+++ src/util/openAPI/kobis.py
@@ -1,26 +1,31 @@
+import json
 import requests
 
 from src.core import settings
 
 
 def get_movie_details(movie_cd):
-    response = requests.get(
-        url = settings.API_URL + '?' + \
-            f'key={settings.API_KEY}' + '&' + \
-                f'movieCD={movie_cd}'
-    )
+    try:
+        url = settings.API_URL + '?key=' + settings.API_KEY + '&movieCd=' + \
+            movie_cd
+                    
+        response = requests.get(url=url).json()
+                
+        movie_info = response['movieInfoResult']['movieInfo']
+        
+        nations = [ nation['nationNm'] for nation in movie_info['nations'] ]
+        genres = [ genre['genreNm'] for genre in movie_info['genres'] ]
+        directors = [ director['peopleNm'] for director in movie_info['directors'] ]
+        actors = [ actor['peopleNm'] for actor in movie_info['actors'] ]
+        
+        movie = {
+            'nation': nations,
+            'genres': genres,
+            'director': directors,
+            'actors': actors
+        }
     
-    movie_info = response['movieInfoResult']['movieInfo']
-    
-    genres = [ genre['genreNm'] for genre in movie_info['genres'] ]
-    directors = [ director['peopleNm'] for director in movie_info['directors'] ]
-    actors = [ actor['peopleNm'] for actor in movie_info['actors'] ]
-    
-    movie = {
-        'nation': movie_info['nations']['nationNm'],
-        'genres': genres,
-        'director': directors,
-        'actors': actors
-    }
-    
-    return movie
+        return movie
+
+    except Exception as error:
+        print(error)
